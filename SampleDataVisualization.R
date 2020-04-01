4937c2225ab136f23b68726ec4ea334a81f0b09c
Nitzan Sherman <nitzan.h.sherman@gmail.com>
2020-03-31 22:10:59 -0400

    completed initial

--- SampleDataVisualization.R
+++ SampleDataVisualization.R
@@ -1,1 +1,12 @@
 #sample code
+library(tidyverse)
+library(RColorBrewer)
+options(scipen=999)
+data("midwest",package = "ggplot2")
+ggplot(midwest,aes(x = area,y = poptotal)) + 
+  geom_point(aes(col = state),size = 3) + 
+  geom_smooth(method = "lm",col = "firebrick",size = 2) +
+  coord_cartesian(xlim=c(0,0.1), ylim=c(0, 1000000)) +
+  labs(title = "Area vs Population",subtitle = "from midwest dataset",y = "Population",x = "Area",
+          caption = "Midwest Demographics") +
+  scale_color_brewer(palette = "Set1")
